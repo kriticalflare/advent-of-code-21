e6b68801f41a4480a85eb9548f6d198730ccdd2d
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2021-12-05 23:50:23 +0530

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,35 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun part1(input: List<Int>): Int {
+        var increase = 0
+        for (curr in input.indices) {
+            if (curr != 0) {
+                val prev = curr - 1
+                if (input[curr] > input[prev]) {
+                    increase++
+                }
+            }
+        }
+        return increase
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var window = 0
+        var increases = 0
+        for (i in 0..2) {
+            window += input[i]
+        }
+        for (i in 3 until input.size) {
+            val prev = window
+            val curr = window - input[i-3] + input[i]
+            if(curr > prev){
+                increases++
+            }
+        }
+        return increases
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInt("Day01")
     println(part1(input))
     println(part2(input))
 }
